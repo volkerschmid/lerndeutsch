6d39d9f1c5d987e86451cacc26cd899b9e5e2f75
Volker Schmid <volker.schmid@lmu.de>
2021-04-13 20:52:05 +0200

    gradethis Optionen übersetzt.

--- R/onload.R
+++ R/onload.R
@@ -1,2 +1,8 @@
 .onLoad <- function(libname, pkgname) {
-}
+  options(gradethis.pass="{random_praise()} Richtig!")
+  options(gradethis.fail="Leider falsch. {maybe_code_feedback()} {random_encouragement()}")
+  options(gradethis.error_checker.message="Der R-Code ist fehlerhaft:\n\n```\n{.error$message}\n```\n\n\n")
+  options(gradethis.glue_correct_test="{.num_correct}/{.num_total} richtig! {random_praise()}")
+  options(gradethis.glue_incorrect_test="{.num_correct}/{.num_total} richtig! {random_encouragement()}")
+  }
+
