9b3226827d1d0bd9bfa338c488e3252b3eecc3ad
Volker Schmid <volker.schmid@lmu.de>
2021-04-13 20:19:05 +0200

    Schreibe JSON ausgelagert

---
+++ R/install_de_json.R
@@ -0,0 +1,35 @@
+#' Schreibe JSON.Datei mit Übersetzungen
+#'
+#' @return
+#' @export
+#'
+write.de.json <- function() {
+  ## Schreibe json Deutsche Übersetzungen
+  jsonlite::write_json( list(
+    button = list(
+      runcode = "Code ausführen",
+      hint = "Hinweis",
+      hint_plural = "Hinweise",
+      hintnext = "Nächster Hinweis",
+      hintprev = "Vorheriger Hinweis",
+      solution =  "Lösung",
+      copyclipboard = "In Zwischenablage kopieren",
+      startover = "Von vorne",
+      continue = "Fortsetzen",
+      submitanswer = "Antwort absenden",
+      previoustopic = "Vorheriges Thema",
+      nexttopic = "Nächstes Thema",
+      questiontryagain = "Nochmals versuchen"
+    ),
+    text = list(
+      startover = "Von vorne",
+      areyousure = "Sind Sie sicher, dass Sie neu beginnen möchten? (alle Übungsfortschritte werden zurückgesetzt)?",
+      youmustcomplete = "Sie müssen die",
+      exercise = "Aufgabe",
+      exercise_plural ="Aufgaben",
+      inthissection = "in diesem Abschnitt abschließen, bevor Sie fortfahren.",
+      code = "Code",
+      quiz = "Quiz")
+  ), path="de.json", auto_unbox=TRUE)
+
+}
--- Readme.md
+++ Readme.md
@@ -1,5 +1,33 @@
 ## lernedeutsch: Deutsche Oberfläche für learnr und gradethis
 
-``` r
-remotes::install_github("volkerschmid/lernedeutsch")
+### Installation
+
+```
+remotes::install_github("volkerschmid/lerndeutsch")
+```
+
+### Einmalig
+
+Schreibe deutsche JSON-Datei:
+
+```
+write.de.json
+```
+
+## Benutzung
+
+```
+output: 
+  learnr::tutorial:
+    language: de.json
+runtime: shiny_prerendered
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+library(learnr)
+library(gradethis)
+library(lerndeutsch)
+```
+
 ```
